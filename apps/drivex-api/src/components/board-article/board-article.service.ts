2ff108d794d31a6ae175f18c4980f534e4af3da5
SirojAli <siroj.origin.profile@gmail.com>
2025-05-25 12:21:58 +0900

    feat: Develop all Article related GraphQL API business logic

--- apps/drivex-api/src/components/board-article/board-article.service.ts
+++ apps/drivex-api/src/components/board-article/board-article.service.ts
@@ -29,101 +29,100 @@ export class BoardArticleService {
 		private readonly likeService: LikeService,
 	) {}
 
-	// public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
-	// 	input.memberId = memberId;
-	// 	try {
-	// 		const result = await this.boardArticleModel.create(input);
-	// 		await this.memberService.memberStatsEditor({
-	// 			_id: memberId,
-	// 			targetKey: 'memberArticles',
-	// 			modifier: 1,
-	// 		});
-	// 		return result;
-	// 	} catch (err) {
-	// 		console.log('Error, Service.model:', err.message);
-	// 		throw new BadRequestException(Message.CREATE_FAILED);
-	// 	}
-	// }
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberArticles',
+				modifier: 1,
+			});
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 
-	// public async getBoardArticle(memberId: ObjectId, articleId: ObjectId): Promise<BoardArticle> {
-	// 	const search: T = {
-	// 		_id: articleId,
-	// 		articleStatus: BoardArticleStatus.ACTIVE,
-	// 	};
+	public async getBoardArticle(memberId: ObjectId, articleId: ObjectId): Promise<BoardArticle> {
+		const search: T = {
+			_id: articleId,
+			articleStatus: BoardArticleStatus.ACTIVE,
+		};
 
-	// 	const targetBoardArticle: BoardArticle = await this.boardArticleModel.findOne(search).lean().exec();
-	// 	if (!targetBoardArticle) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		const targetBoardArticle: BoardArticle = await this.boardArticleModel.findOne(search).lean().exec();
+		if (!targetBoardArticle) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-	// 	if (memberId) {
-	// 		const viewInput = { memberId: memberId, viewRefId: articleId, viewGroup: ViewGroup.ARTICLE };
-	// 		const newView = await this.viewService.recordView(viewInput);
-	// 		if (newView) {
-	// 			await this.boardArticleStatsEditor({ _id: articleId, targetKey: 'articleViews', modifier: 1 });
-	// 			targetBoardArticle.articleViews++;
-	// 		}
-	// 		// meLiked
-	// 		const likeInput = { memberId: memberId, likeRefId: articleId, likeGroup: LikeGroup.ARTICLE };
-	// 		targetBoardArticle.meLiked = await this.likeService.checkLikeExistence(likeInput);
-	// 	}
-	// 	targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);
-	// 	return targetBoardArticle;
-	// }
+		if (memberId) {
+			const viewInput = { memberId: memberId, viewRefId: articleId, viewGroup: ViewGroup.ARTICLE };
+			const newView = await this.viewService.recordView(viewInput);
+			if (newView) {
+				await this.boardArticleStatsEditor({ _id: articleId, targetKey: 'articleViews', modifier: 1 });
+				targetBoardArticle.articleViews++;
+			}
+			// meLiked
+			const likeInput = { memberId: memberId, likeRefId: articleId, likeGroup: LikeGroup.ARTICLE };
+			targetBoardArticle.meLiked = await this.likeService.checkLikeExistence(likeInput);
+		}
+		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);
+		return targetBoardArticle;
+	}
 
-	// public async updateBoardArticle(memberId: ObjectId, input: BoardArticleUpdate): Promise<BoardArticle> {
-	// 	const { _id, articleStatus } = input;
+	public async updateBoardArticle(memberId: ObjectId, input: BoardArticleUpdate): Promise<BoardArticle> {
+		const { _id, articleStatus } = input;
 
-	// 	const result = await this.boardArticleModel
-	// 		.findOneAndUpdate({ _id: _id, memberId: memberId, articleStatus: BoardArticleStatus.ACTIVE }, input, {
-	// 			new: true,
-	// 		})
-	// 		.exec();
-	// 	if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		const result = await this.boardArticleModel
+			.findOneAndUpdate({ _id: _id, memberId: memberId, articleStatus: BoardArticleStatus.ACTIVE }, input, {
+				new: true,
+			})
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
-	// 	if (articleStatus === BoardArticleStatus.DELETE) {
-	// 		await this.memberService.memberStatsEditor({
-	// 			_id: memberId,
-	// 			targetKey: 'memberArticles',
-	// 			modifier: -1,
-	// 		});
-	// 	}
-	// 	return result;
-	// }
+		if (articleStatus === BoardArticleStatus.DELETE) {
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberArticles',
+				modifier: -1,
+			});
+		}
+		return result;
+	}
 
-	// public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): Promise<BoardArticles> {
-	// 	const { articleCategory, text } = input.search;
-	// 	const match: T = { articleStatus: BoardArticleStatus.ACTIVE };
-	// 	const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+	public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): Promise<BoardArticles> {
+		const { articleCategory, text } = input.search;
+		const match: T = { articleStatus: BoardArticleStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-	// 	if (articleCategory) match.articleCategory = articleCategory;
-	// 	if (text) match.articleTitle = { $regex: new RegExp(text, 'i') };
-	// 	if (input.search?.memberId) {
-	// 		match.memberId = shapeIntoMongoObjectId(input.search.memberId);
-	// 	}
-	// 	console.log('match:', match);
+		if (articleCategory) match.articleCategory = articleCategory;
+		if (text) match.articleTitle = { $regex: new RegExp(text, 'i') };
+		if (input.search?.memberId) {
+			match.memberId = shapeIntoMongoObjectId(input.search.memberId);
+		}
+		console.log('match:', match);
 
-	// 	const result = await this.boardArticleModel
-	// 		.aggregate([
-	// 			{ $match: match },
-	// 			{ $sort: sort },
-	// 			{
-	// 				$facet: {
-	// 					list: [
-	// 						{ $skip: (input.page - 1) * input.limit },
-	// 						{ $limit: input.limit },
-	// 						// meLiked
-	// 						lookupAuthMemberLiked(memberId),
-	// 						lookupMember,
-	// 						{ $unwind: '$memberData' },
-	// 					],
-	// 					metaCounter: [{ $count: 'total' }],
-	// 				},
-	// 			},
-	// 		])
-	// 		.exec();
-	// 	console.log('result:', result);
-	// 	if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-	// 	return result[0];
-	// }
+		const result = await this.boardArticleModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							lookupAuthMemberLiked(memberId),
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		console.log('result:', result);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
+	}
 
 	// public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle> {
 	// 	const target: BoardArticle = await this.boardArticleModel
@@ -152,68 +151,68 @@ export class BoardArticleService {
 	// 	return result;
 	// }
 
-	// //** ADMIN **//
-	// public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
-	// 	const { articleStatus, articleCategory } = input.search;
-	// 	const match: T = {};
-	// 	const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+	//** ADMIN **//
+	public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
+		const { articleStatus, articleCategory } = input.search;
+		const match: T = {};
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-	// 	if (articleStatus) match.articleStatus = articleStatus;
-	// 	if (articleCategory) match.articleCategory = articleCategory;
+		if (articleStatus) match.articleStatus = articleStatus;
+		if (articleCategory) match.articleCategory = articleCategory;
 
-	// 	const result = await this.boardArticleModel
-	// 		.aggregate([
-	// 			{ $match: match },
-	// 			{ $sort: sort },
-	// 			{
-	// 				$facet: {
-	// 					list: [
-	// 						{ $skip: (input.page - 1) * input.limit },
-	// 						{ $limit: input.limit },
-	// 						lookupMember,
-	// 						{ $unwind: '$memberData' },
-	// 					],
-	// 					metaCounter: [{ $count: 'total' }],
-	// 				},
-	// 			},
-	// 		])
-	// 		.exec();
-	// 	console.log('result:', result);
-	// 	if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-	// 	return result[0];
-	// }
+		const result = await this.boardArticleModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		console.log('result:', result);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
+	}
 
-	// public async updateBoardArticleByAdmin(input: BoardArticleUpdate): Promise<BoardArticle> {
-	// 	let { _id, articleStatus } = input;
+	public async updateBoardArticleByAdmin(input: BoardArticleUpdate): Promise<BoardArticle> {
+		let { _id, articleStatus } = input;
 
-	// 	const result = await this.boardArticleModel
-	// 		.findOneAndUpdate({ _id: _id, articleStatus: BoardArticleStatus.ACTIVE }, input, { new: true })
-	// 		.exec();
-	// 	if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+		const result = await this.boardArticleModel
+			.findOneAndUpdate({ _id: _id, articleStatus: BoardArticleStatus.ACTIVE }, input, { new: true })
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 
-	// 	if (articleStatus === BoardArticleStatus.DELETE) {
-	// 		await this.memberService.memberStatsEditor({
-	// 			_id: result.memberId,
-	// 			targetKey: 'memberArticles',
-	// 			modifier: -1,
-	// 		});
-	// 	}
-	// 	return result;
-	// }
+		if (articleStatus === BoardArticleStatus.DELETE) {
+			await this.memberService.memberStatsEditor({
+				_id: result.memberId,
+				targetKey: 'memberArticles',
+				modifier: -1,
+			});
+		}
+		return result;
+	}
 
-	// public async removeBoardArticleByAdmin(articleId: ObjectId): Promise<BoardArticle> {
-	// 	const search: T = { _id: articleId, articleStatus: BoardArticleStatus.DELETE };
-	// 	const result = await this.boardArticleModel.findOneAndDelete(search).exec();
-	// 	if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
-	// 	return result;
-	// }
+	public async removeBoardArticleByAdmin(articleId: ObjectId): Promise<BoardArticle> {
+		const search: T = { _id: articleId, articleStatus: BoardArticleStatus.DELETE };
+		const result = await this.boardArticleModel.findOneAndDelete(search).exec();
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+		return result;
+	}
 
-	// //** Additional Logics **//
-	// public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
-	// 	console.log('executed');
-	// 	const { _id, targetKey, modifier } = input;
-	// 	return await this.boardArticleModel
-	// 		.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true })
-	// 		.exec();
-	// }
+	//** Additional Logics **//
+	public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
+		console.log('executed');
+		const { _id, targetKey, modifier } = input;
+		return await this.boardArticleModel
+			.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true })
+			.exec();
+	}
 }
